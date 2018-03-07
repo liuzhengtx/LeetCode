31e9568e0d9ab5af85fa64a21ce58f3c6f1649c0
liuzhengtx <2575046221@qq.com>
2018-03-07 20:41:29 +0800

    Add Two Numbers

---
+++ AddTwoNum .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	int i;
+	for(i=0;i<10;i++)
+	{
+		i++;
+		printf("%d ",i);	
+	}
+	for(i=0;i<10;i++)
+	{
+	++i;
+	printf("%d ",i);
+		
+	}
+	
+	return 0;
+ } 
